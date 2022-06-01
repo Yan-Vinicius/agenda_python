35f54dcb6243aec9fbd002a1a185e5fe66231449
Yan Vinícius <yan.vinicius.queiroz@gmail.com>
2022-06-01 16:53:06 -0300

    Inclusão da tela e da função de criar um novo evento para a agenda

--- agenda/urls.py
+++ agenda/urls.py
@@ -22,9 +22,12 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
     #Indica um POST (com barra indica um GET)
     path('login/submit', views.submit_login),
-    path('logout/', views.logout_user)
+    path('logout/', views.logout_user),
+
 ]
